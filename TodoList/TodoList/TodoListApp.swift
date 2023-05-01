4b9e7e8150a9826b9f34082d0f930aa9360f00ee
FADEL SULTAN <srqa3h.com@gmail.com>
2023-05-01 18:23:01 +0300

    First Project
    TodoList project listen one

---
+++ TodoList/TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Fadel Sultan on 11/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
