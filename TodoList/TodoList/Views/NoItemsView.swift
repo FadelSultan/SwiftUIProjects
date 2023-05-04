da2da5cb90e40a397f1d9d5250d291370566090d
FADEL SULTAN <srqa3h.com@gmail.com>
2023-05-04 08:32:29 +0300

    Finished lesson 5

--- TodoList/TodoList/Views/NoItemsView.swift
+++ TodoList/TodoList/Views/NoItemsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct NoItemsView: View {
     
     @State private var isAnimated:Bool = false
+    private let secondaryAccentColor:Color = Color("secondaryAccentColor")
     
     var body: some View {
         ScrollView {
@@ -18,18 +19,27 @@ struct NoItemsView: View {
                     .font(.title)
                     .fontWeight(.semibold)
                 Text("Are you a productive person? I think you should click the add button and add a bunch of items to your todo list! 😇")
+                    .padding(.bottom , 40)
                 
                 NavigationLink {
-                    
+                    AddView()
                 } label: {
                     Text("Add Your Task 😎".capitalized)
                         .foregroundColor(.white)
                         .font(.headline)
                         .frame(height: 55)
                         .frame(maxWidth: .infinity)
-                        .background(isAnimated ? Color.accentColor : Color.red)
+                        .background(isAnimated ? secondaryAccentColor : Color.accentColor)
                         .cornerRadius(10)
                 }
+                .padding(.horizontal , isAnimated ? 30 : 50)
+                .shadow(
+                    color: isAnimated ? secondaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
+                    radius: isAnimated ? 30 : 10,
+                    y: isAnimated ? 50 : 30
+                )
+                .scaleEffect(isAnimated ? 1.1 : 1.0)
+                .offset(y: isAnimated ? -7 : 0)
 
             }
             .multilineTextAlignment(.center)
