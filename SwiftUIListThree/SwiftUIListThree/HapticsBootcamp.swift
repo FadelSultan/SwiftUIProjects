e86bc89e20b2f36e52428e93534de3088abe6ede
FADEL SULTAN <srqa3h.com@gmail.com>
2023-05-09 20:08:58 +0300

    Lesson 10

--- SwiftUIListThree/SwiftUIListThree/HapticsBootcamp.swift
+++ SwiftUIListThree/SwiftUIListThree/HapticsBootcamp.swift
@@ -15,20 +15,32 @@ class HapticsManager {
         generator.notificationOccurred(type)
     }
     
-    func impact() {
+    func impact(style:UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
         
+        generator.impactOccurred()
     }
-    
 }
 
 struct HapticsBootcamp: View {
+    
     var body: some View {
-        VStack {
-            Button("Success") {
-                HapticsManager.instance.notification(type: .success)
-            }
+        
+        VStack(spacing:40) {
+            Button("Success") {HapticsManager.instance.notification(type: .success)}
+            Button("warning") {HapticsManager.instance.notification(type: .warning)}
+            Button("error") {HapticsManager.instance.notification(type: .error)}
+            
+            Divider()
+            
+            Button("heavy") {HapticsManager.instance.impact(style: .heavy)}
+            Button("light") {HapticsManager.instance.impact(style: .light)}
+            Button("medium") {HapticsManager.instance.impact(style: .medium)}
+            Button("rigid") {HapticsManager.instance.impact(style: .rigid)}
+            Button("soft") {HapticsManager.instance.impact(style: .soft)}
         }
     }
+    
 }
 
 struct HapticsBootcamp_Previews: PreviewProvider {
