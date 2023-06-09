ee40de1bfdd4f58f4b4253026cfc005fe7dbcf9b
FADEL SULTAN <srqa3h.com@gmail.com>
2023-06-09 16:43:18 +0300

    Session 18

---
+++ Crypto/Crypto/Core/Detail/Views/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  Crypto
+//
+//  Created by Fadel Sultan on 20/11/1444 AH.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
