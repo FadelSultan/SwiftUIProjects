07213596431aec0d8a395e2048fc3b69a85b14cf
FADEL SULTAN <srqa3h.com@gmail.com>
2023-06-11 13:41:41 +0300

    Lesson 20

--- Crypto/Crypto/Core/Detail/Views/DetailView.swift
+++ Crypto/Crypto/Core/Detail/Views/DetailView.swift
@@ -24,20 +24,68 @@ struct DetailView: View {
     
     @StateObject var vm:CoinDetailViewModel
     
+    private let column:[GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
+    private let spacing:CGFloat = 30
+    
     init(coin: CoinModel) {
         _vm = StateObject(wrappedValue: CoinDetailViewModel(coin: coin))
         print("Initailaizing for detail view \(coin.name) ")
     }
     
     var body: some View {
-        ZStack {
-            Text(vm.coinDetail?.id ?? "")
+        ScrollView {
+            VStack(spacing:20) {
+                Text("")
+                    .frame(height: 150)
+                
+                Text("Overview")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(Color.theme.accent)
+                    .frame(maxWidth: .infinity , alignment: .leading)
+                
+                Divider()
+                
+                LazyVGrid(
+                    columns: column,
+                    alignment: .leading ,
+                    spacing: spacing) {
+                        ForEach(0..<6) { _ in
+                            StatisticView(stat: StatisticModel(title: "Title", value: "Value"))
+                        }
+                    }
+                
+                Text("Additional")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(Color.theme.accent)
+                    .frame(maxWidth: .infinity , alignment: .leading)
+                
+                Divider()
+                
+                LazyVGrid(
+                    columns: column,
+                    alignment: .leading ,
+                    spacing: spacing) {
+                        ForEach(0..<6) { _ in
+                            StatisticView(stat: StatisticModel(title: "Title", value: "Value"))
+                        }
+                    }
+                
+            }
+            .padding()
         }
+        .navigationTitle(vm.coin.name)
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(coin: dev.coin)
+        NavigationView {
+            DetailView(coin: dev.coin)
+        }
     }
 }
